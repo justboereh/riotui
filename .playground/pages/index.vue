9e5d0aca367b35db1444acc4317d6f8e1180f8d6
Boe Reh <justboereh@gmail.com>
2023-10-03 11:26:04 -0500

    first commit

---
+++ .playground/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <rui-button> default button </rui-button>
+  <rui-button type="primary"> primary button </rui-button>
+  <rui-button type="text"> text button </rui-button>
+</template>
---
+++ components/rui-button.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+type Size = "tiny" | "small" | "default" | "medium" | "large";
+type Type = "default" | "primary" | "dashed" | "text" | "link";
+
+type Props = {
+  color?: string;
+  size?: Size;
+  type?: Type;
+};
+
+const Types: Record<Type, string> = {
+  default: "",
+  primary: "",
+  dashed: "",
+  text: "",
+  link: "",
+};
+
+const Sizes: Record<Size, string> = {
+  default: "",
+  tiny: "",
+  small: "",
+  medium: "",
+  large: "",
+};
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <button :class="[]">
+    <slot />
+  </button>
+</template>
